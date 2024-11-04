5538b51e463cc03dc31a74210a1b65466fd01d31
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-04 21:42:25 +0200

    Changed the self.pi --> Circle.pi

--- class-circle.py
+++ class-circle.py
@@ -1,13 +1,13 @@
 class Circle():
 
-  pi = 3.14
+  pi = 3.14 # Class Object Attribute
 
   def __init__(self, radius=1):
     self.radius = radius
     self.area = radius * radius * self.pi
 
-  def get_circumference(self):
-    return self.radius * self.pi * 2 
+  def get_circumference(self): # Method
+    return self.radius * Circle.pi * 2 
   
 
 my_circle = Circle() #instance of the class Circle
