d766381d30783423b4812b4514b95f2afc769462
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-04 22:59:14 +0200

    created a class Book

--- special-methods.py
+++ special-methods.py
@@ -1,1 +1,10 @@
 # Special methods - Allow us to use some built in operations in python such as length or the print function with our own user created object.
+
+
+class Book():
+
+  def __init__(self, title,author, pages):
+    
+    self.title = title
+    self.author = author
+    self.pages = pages
