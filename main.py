0958dfb7205272d920be922a2bf4eb7f531b9588
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-04 20:35:01 +0200

    Create more attributes for our class

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 class Dog():
-  def __init__(self,breed):
+  def __init__(self,breed, name, spots):
     self.breed = breed 
+    self.name = name
 
-my_dog = Dog(breed='Pitbull')
+    #Spots is a boolean
+    self.spots = spots
+# Attributes breed, name , spots
+#We take in an argument breed, name , spots
+#Assign it using self.attribute_name/argument
+
+my_dog = Dog(breed='Unknown', name="Chopper",spots=False)
 
 
 print(my_dog.breed)
+print(my_dog.name)
+print(my_dog.spots)
