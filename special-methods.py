b9ac0cc82a05bd51f9eb14999d46837c6aacb575
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-04 23:12:21 +0200

    Created special methods that are commonly used

--- special-methods.py
+++ special-methods.py
@@ -9,10 +9,19 @@ class Book():
     self.author = author
     self.pages = pages
 
-  def __str__(self):
+  def __str__(self): # Special method that gives the string representation of the class
     return f"{self.title} by {self.author}"
+  
+  def __len__(self): # allows you to declare the length of the class Book
+    return self.pages
+  
+  def __del__(self):
+    print(f"{self.title} has been deleted!")
   
 
 buku = Book('Journey to manhood', 'Nsuku', 240)
 
 print(buku)
+str(buku)
+len(buku)
+del buku
