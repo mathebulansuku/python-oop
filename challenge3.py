4decad7c412044438973e5697a9712fbe2cf486d
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-08 23:04:44 +0200

    Create a new challenge3.py file. In this challenge I should create a bank account

---
+++ challenge3.py
@@ -0,0 +1,1 @@
+# For this challenge, create a bank account class that has 2 attributes: Owner and balance. 2 Methods: deposit and withdraw. Withdrawal amount should not excedd the available balance.
