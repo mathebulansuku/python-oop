7304693c66f6c470f468e84106cb6ff0fe5c37bf
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-08 22:50:37 +0200

    Created attributes of the cylinder

---
+++ challenge2.py
@@ -0,0 +1,11 @@
+class Cylinder:
+
+  def __init__(self, height=1, radius=1):
+    self.height = height
+    self.radius = radius
+
+  def volume(self):
+    pass
+
+  def surface_area(self):
+    pass
