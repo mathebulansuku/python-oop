45f6de7f5ae7cbf0b933e5e77af6b053b9e0827d
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-11-08 22:41:51 +0200

    Broke down the coordinates using tuple unpacking

--- challenge1.py
+++ challenge1.py
@@ -1,14 +1,26 @@
 # Fill in the line class methods to accept coordinates as a pair of tuples and return the slope and distance of t
 
-
 class Line:
 
   def __init__(self,coor1,coor2):
 
-    self.coor1 =(coor1, coor2)
+    self.coor1 =coor1
+    self.coor2 =coor2
 
   def distance(self):
-      return ((self.coor1 - self.coor2) ** 2 + (self.coor1 - self.coor2) ** 2) ** 0.5
+      x1,y1 = self.coor1
+      x2,y2 = self.coor2
+
+
+      return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
   def slope(self):
-    pass
+      
+      x1,y1 = self.coor1
+      x2,y2 = self.coor2
+
+      return (y2-y1)/(x2-x1)
+  
+
+  a1 = (3,2)
+  a2 = (8,10)
